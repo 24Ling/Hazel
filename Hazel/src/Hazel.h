16e470ff296d3ca0224c07949a840c25ec94605f
4 2 <24Ling>
2022-04-23 21:00:17 +0800

    Log Module updata

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 //for use by Hazel Application
 
 #include"Hazel/Application.h"
+#include"Hazel/Log.h"
 
 //---EntryPoint.h-------------//
 #include"Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -3,6 +3,12 @@
 extern Hazel::Application* Hazel::CreateApplication();
 int main(int argc,char ** argv)
 {
+	/*Hazel::Log l;
+	l.Print();*/
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var = {0}", a);
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
